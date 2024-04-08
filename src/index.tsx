84ec8bde91eb1add0c5e2435c7b7322e377863ee
iluvator <petrov34rus@yandex.ru>
2024-04-09 00:05:39 +0300

    Проблема 2

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,11 @@ import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import 'react-toastify/dist/ReactToastify.css';
+<<<<<<< Updated upstream
+=======
+import HistoryRouter from './components/history-route/history-route';
+import browserHistory from './browser-history';
+>>>>>>> Stashed changes
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
